feb70e80f4d28de7ee950550566baab83629caeb
Timur-Rustamov <84975945+Timur-Rustamov@users.noreply.github.com>
2021-12-06 17:47:20 +0300

    Delete .vagrant directory

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["ffi",[">= 1.0.0"]],["win32-process",[">= 0"]],["vagrant-sshfs",["= 1.3.6"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"a4fc4989ab128ff9ef440deb5267bb51325360f267b676e846a9a0c953218813","vagrant_version":"2.2.18"}
